15c4f802e6240b46a758b24ab9309f0ea6821c9e
remarkablejames <lascasasjames85@gmail.com>
2022-04-22 09:17:18 +0200

    Added an action creator to the PostList component

---
+++ src/components/PostList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost } from "../actions";
+class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPost();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>PostList</h1>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchPost })(PostList);
